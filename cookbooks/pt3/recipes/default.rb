3f2053943887066180d4b8018e01c98b82bb395d
Kiichiro NAKA <naka@i3-systems.com>
2013-04-01 00:14:49 +0900

    Added pt3 recipe

--- cookbooks/pt3/recipes/default.rb
+++ cookbooks/pt3/recipes/default.rb
@@ -7,13 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
-template "watchdog" do |
-  path "/etc/default/watchdog"
-  source "watchdog.erb"
+# git — Chef Docs http://docs.opscode.com/resource_git.html
+git "#{Chef::Config[:file_cache_path]}/pt3" do
+  repository "/arc/repos/pt3.git"
+  reference "master"
+  action :sync
+  notifies :run, "bash[install]"
+end
+
+bash "install" do
+  cwd "#{Chef::Config[:file_cache_path]}/pt3"
+  code <<-EOH
+    make install
+    rmmod pt3_drv
+    modprobe pt3_drv
+  EOH
+  action :nothing
+end
+
+bash "module" do
+  not_if "grep pt3_drv /etc/modules"
+  code <<-EOC
+    echo pt3_drv >> /etc/modules
+  EOC
+end
+
+template "pt3.conf" do
+  path "/etc/modprobe.d/pt3.conf"
+  source "pt3.conf.erb"
   owner "root"
   group "root"
   mode "0644"
 end
 
-watchdog_module softdog
-watchdog_options= --softboot
